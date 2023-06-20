f8e74205359afa360d7a2ed26a3caeaebcd2244a
Juan Ferraro <juansebastianferraro@gmail.com>
2023-06-20 13:33:43 -0500

    Finalización de curso: proyecto y visualización de grafica por pais o mundial.

--- app/main.py
+++ app/main.py
@@ -5,13 +5,17 @@ import charts
 def run():
     #Obtener data:
     data = read_csv.read_csv('./app/data.csv')
-    country = input('Cual pais?: ')
+    data = list(filter(lambda item: item['Continent'] == 'South America', data))
+    countries = list(map(lambda x: x['Country/Territory'], data))
+    percentages = list(map(lambda x:x['World Population Percentage'], data))
+    charts.generate_pie_chart(countries, percentages)    
+    """ country = input('Cual pais?: ')
 
     result = utils.population_by_country(data, country)
     if len(result) > 0:
         country = result[0]
         keys, values = utils.get_population(country)
-        charts.generate_bar_chart(keys, values)
+        charts.generate_bar_chart(keys, values) """
 
 if __name__ == '__main__':
     run()
