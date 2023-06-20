9c41a1680527927ceedec062c2a2c3d87a1fcc64
Juan Ferraro <juansebastianferraro@gmail.com>
2023-06-20 10:09:24 -0500

    Clases: leer csv, graficar a partir de csv con matplolib y grafica de proyecto de curso

--- app/main.py
+++ app/main.py
@@ -1,22 +1,17 @@
 import utils
+import read_csv
+import charts
 
 def run():
-    keys, values = utils.get_population()
-    print(keys, values)
+    #Obtener data:
+    data = read_csv.read_csv('./app/data.csv')
     country = input('Cual pais?: ')
-    result = utils.population_by_country(data, country)
-    print(result)
 
-data = [
-        {
-            'Country': 'Colombia',
-            'Population': 300
-        },
-        {
-            'Country': 'Venezuela',
-            'Population': 200
-        }
-    ]
+    result = utils.population_by_country(data, country)
+    if len(result) > 0:
+        country = result[0]
+        keys, values = utils.get_population(country)
+        charts.generate_bar_chart(keys, values)
 
 if __name__ == '__main__':
     run()
